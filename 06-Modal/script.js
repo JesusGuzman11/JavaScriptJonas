3fb461293953053768bd5ce18eb2f3c09a667efd
Jesus Guzman <jesusguzman.0311@gmail.com>
2021-03-12 12:05:33 -0600

    Modal

---
+++ 06-Modal/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// NOTE: ClassList allows manipulations of classes add/remove/check
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  const element = btnsOpenModal[i];
+  element.addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
