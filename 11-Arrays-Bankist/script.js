97ef2fb854466bf42c33cb5b89f1f4e2607b1a7b
Jesus Guzman <jesusguzman.0311@gmail.com>
2021-04-03 21:25:33 -0600

    For Each

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -71,6 +71,70 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// NOTE:
+// Slice returns a shallow copy of array if there's no arguments
+// also recieve numbers as arguments
+
+// const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+// console.log(arr.slice(2)); // ["c", "d", "e", "f"]
+// console.log(arr.slice(2, 4)); // ["c", "d"]
+// console.log(arr.slice(-2)); // ["e", "f"]
+// console.log(arr.slice(-1)); // ["f"]
+// console.log(arr.slice(1, -2)); // ["b", "c", "d"]
+// console.log(arr.slice()); // Shallow copy of array
+// console.log([...arr]); // Shallow copy of array
+
+// NOTE:
+// Splice returns a array of the elements deleted from an array but mutates the original
+// works similar to slice method the second parameter is a number of elements
+// that we want to delete from splice array and it will also keep it on original array
+
+// const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+// console.log(arr.splice(-1)); // ["f"]
+// console.log(arr); // ["a", "b", "c", "d", "e"]
+// console.log(arr.splice(1, 2)); // ["b", "c"]
+// console.log(arr); // ["a", "d", "e"]
+
+// NOTE:
+// Reverse mutates the original array
+
+// const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+// console.log(arr.reverse()); // ["f", "e", "d", "c", "b", "a"]
+// console.log(arr); // ["f", "e", "d", "c", "b", "a"]
+
+// NOTE:
+// Concat merge 2 arrays and then return a shallow copy of them
+// so doesnt mutate the arrays
+
+// const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+// const arr2 = ['g', 'h', 'i'];
+
+// const letters = arr.concat(arr2);
+// console.log(letters); // ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+// console.log(arr); // ["a", "b", "c", "d", "e", "f"]
+// console.log([...arr, ...arr2]); // ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+
+// NOTE:
+// Join add all elements of the array into one string separated by the argument
+// const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+// console.log(arr.join(' - ')); // a - b - c - d - e - f
+
+// NOTE:
+// For Each loops over arrays and our callback recieve the
+// element, index and the array we're looping over
+// you can't use continue and break on this method
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// movements.forEach((move, idx, array) => {
+//   if (move > 0) {
+//     console.log(`Movement ${idx} You deposited: ${move}`);
+//   } else {
+//     console.log(`Movement ${idx} You withdrew: ${Math.abs(move)}`);
+//   }
+//   console.log(array); // movements array
+// });
