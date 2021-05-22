12aeddb6f2bc1c11b628216c50fa6957359e5130
Jesus Guzman <jesusguzman.0311@gmail.com>
2021-05-22 15:53:28 -0500

    Finished JS course

---
+++ 18-Forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b29a4126-6d80-4e28-9006-d39d9a56c792';
+export const MODAL_CLOSE_SEC = 2.5;
